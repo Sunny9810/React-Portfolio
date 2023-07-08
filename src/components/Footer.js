d4437ae18bca294ec9c34b3dd30fc16923dccd13
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-07-08 15:47:31 -0400

    adding footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,3 +18,39 @@ const socials = [
     color: "#2EACE8",
   },
 ];
+//Footer component
+const Footer = () => {
+  return (
+    <footer className="footer bg-dark text-white">
+      <div className="footer-container">
+        <section className="social-icons-wrapper d-flex flex-md-row justify-content-center">
+          {socials.map(({ name, url, icon, color }) => (
+            <div
+              key={name}
+              className="d-flex flex-column align-items-center mx-md-2 transparent-bg"
+            >
+             <a
+                href={url}
+                className={`icon ${name}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={icon}
+                  bounce
+                  className="footer-icon-size" 
+                  style={{ color: color }}
+                />
+              </a>
+              <a href={url} target="_blank" rel="noreferrer">
+                <span className="d-inline-block mx-1 name">{name}</span>
+              </a>
+            </div>
+          ))}  
+          </section>
+             </div>
+    </footer>
+  );
+};  
+// Export Footer
+export default Footer;
