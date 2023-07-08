95f25b0e4519214f0c8a88b42ad6474dc195609d
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-07-08 15:16:18 -0400

    rendering the contact form,creating validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,5 +64,75 @@ function Contact() {
         setMessageSent(true);
       }
     };
+    // Render the contact form
+  return (
+    <section id="contact">
+      <Container>
+        <Row>
+          <Col lg={8} className="mx-auto">
+            <h2 className="contact-header">Contact Me</h2>
+            {messageSent && (
+              <div className="alert alert-success" role="alert">
+                Your message has been sent successfully!
+              </div>
+            )}
+            <Form onSubmit={handleSubmit} noValidate>
+              <Form.Group>
+                <Form.Label htmlFor="name">Name:</Form.Label>
+                <Form.Control
+                  type="text"
+                  className={nameError ? "is-invalid" : ""}
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={handleNameChange}
+                  required
+                />
+                {nameError && (
+                  <div className="invalid-feedback">
+                    Name must be more than 5 characters
+                  </div>
+                )}
+              </Form.Group>
+              <Form.Group>
+                <Form.Label htmlFor="email">Email:</Form.Label>
+                <Form.Control
+                  type="email"
+                  className={emailError ? "is-invalid" : ""}
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                />
+                {emailError && (
+                  <div className="invalid-feedback">
+                    A valid email is required
+                  </div>
+                )}
+              </Form.Group>
+              <Form.Group>
+                <Form.Label htmlFor="message">Message:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  className={messageError ? "is-invalid" : ""}
+                  id="message"
+                  name="message"
+                  rows="5"
+                  value={message}
+                  onChange={handleMsgChange}
+                />
+                {messageError && (
+                  <div className="invalid-feedback">
+                    Message must be at least 150 characters
+                  </div>
+                )}
+              </Form.Group>
+              <Button type="submit" className="custom-send-message-button">
+                Send Message
+              </Button>
+            </Form>
+          </Col>
+        </Row>
 
 
