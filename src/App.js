11deb83145e1a9db04a9600e6a4ca5c83151490f
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-07-07 20:45:15 -0400

    importing all lib and components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// Import necessary React libraries and components
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+
+// Import Bootstrap and custom CSS styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style/App.css";
+
+export default App;
