2de6855bb4613413672446ddd60b77ddb442d872
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-07-08 15:54:39 -0400

    adding headrer

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import Navigation from "./Navigation";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Row, Col } from "react-bootstrap";
+import "../style/Header.css";
+
+// Header component
+function Header() {
+    return (
+      // Header component
+      <>
+        <Container fluid className="bg-dark">
+          <Row className="justify-content-center text-center mb-0">
+            <Col className="pb-0">
+              <h1 className="text-white" style={{ fontSize: "70px" }}>
+              </h1>
+          </Col>
+        </Row>
+      </Container>
+      <Navigation />
+    </>
+  );
+}
+// Export Header
+export default Header;
+
+                
